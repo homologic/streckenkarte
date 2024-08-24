a5072f4a4df00ae99609892188caa4861f02906f
Antonia Pérez-Cerezo <antonia@antonia.is>
2024-08-24 22:31:56 +0200

    bugfix

--- scripts/mapbuilder.sh
+++ scripts/mapbuilder.sh
@@ -7,11 +7,11 @@ temp=$(mktemp -d)
 mkdir "$temp/data"
 for i in "$1/data/"*
 do	
-	ogrmerge.py -single -o "$temp/$i.json"  "$i"/* 
+	ogrmerge.py -single -o "$temp/$(basename $i).json"  "$i"/* 
 done
 
 
-tippecanoe -aN -z10 -o "$temp/strecken.pmtiles" $temp/data/*.json
+tippecanoe -aN -z10 -o "$temp/strecken.pmtiles" $temp/*.json
 
 mv $temp/strecken.pmtiles "$2"
 
